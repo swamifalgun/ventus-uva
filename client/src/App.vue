81c7aeb04ed662105d277027deaaa61f4d1a0f9c
Falgun Swami <swamifalgun@yahoo.com>
2021-05-19 11:09:07 +0300

    Change font-size and color

--- client/src/App.vue
+++ client/src/App.vue
@@ -21,7 +21,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #1E1714;
   margin-top: 60px;
+  font-size: 32px;
 }
 </style>
