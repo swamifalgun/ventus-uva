7b5062446dd6c6996b3d3a12beb164970d6cfb72
Falgun Swami <swamifalgun@yahoo.com>
2021-05-19 10:14:55 +0300

    Fix font in main app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Weather msg="Welcome to Your Vue.js App"/>
+    <Weather />
   </div>
 </template>
 
@@ -17,7 +17,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Courier New', Courier, monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
